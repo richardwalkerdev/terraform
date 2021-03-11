867d2cc0de76555df2241da1a78a95362d532dab
Richard Walker <me@richardwalker.dev>
2021-03-11 13:12:56 +0000

    update

--- prod.tf
+++ prod.tf
@@ -39,6 +39,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-00f0391e303251c1a"
   instance_type = "t2.nano"
   
@@ -51,9 +53,12 @@ resource "aws_instance" "prod_web" {
   }
 }
 
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
 
+resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
