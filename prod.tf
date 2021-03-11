609f418adeed3f6e6886636798ff02b37f9b9dd3
Richard Walker <me@richardwalker.dev>
2021-03-11 13:03:35 +0000

    add elastic IP

--- prod.tf
+++ prod.tf
@@ -37,3 +37,24 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true"
   }
 }
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-00f0391e303251c1a"
+  instance_type = "t2.nano"
+  
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
